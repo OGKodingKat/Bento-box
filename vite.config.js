af1091a6504ed00c307d0adb4d7a9dba10842152
OGKodingKat <katmarie9628@gmail.com>
2024-11-19 13:10:33 -0600

    add mime type to config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     mimeTypes: {
+      'text/javascript': ['js', 'jsx'],
       'application/javascript': ['js', 'jsx']
     }
   }
